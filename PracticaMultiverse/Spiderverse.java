04738146e3f22c877ead67c8c8524938881cdb10
Diego I. Jimenez <99154249+DiegowJmz@users.noreply.github.com>
2022-05-04 12:00:44 -0500

    Update Spiderverse.java

--- PracticaMultiverse/Spiderverse.java
+++ PracticaMultiverse/Spiderverse.java
@@ -1,13 +1,13 @@
 public class Spiderverse {
 
     public static void main(String[] args) {
-        Spiderman andrew = new AdrewSpidy();
+        Spiderman andrew = new AdrewSpidy("Andrew Garlfield","andrew.jpg");
         andrew.mostrarPoderes();
 
-        Spiderman tobey = new TobeySpidy();
+        Spiderman tobey = new TobeySpidy("Tobey Maguire", "tobey.jpg");
         tobey.mostrarPoderes();
 
-        Spiderman tom = new TomSpidy();
+        Spiderman tom = new TomSpidy("Tom Holland", "TomH.jpg");
         tom.mostrarPoderes();
     }
 
