59ca225011df103d998bfd0b413b7317fdfe955c
Diego I. Jimenez <99154249+DiegowJmz@users.noreply.github.com>
2022-05-04 12:01:33 -0500

    Update TomSpidy.java

--- PracticaMultiverse/TomSpidy.java
+++ PracticaMultiverse/TomSpidy.java
@@ -1,14 +1,19 @@
 public class TomSpidy extends Spiderman{
+    String poder;
 
+    public TomSpidy(String nombre, String poder) {
+        super(nombre);
+        this.poder = poder;
+    }
     @Override
     public void mostrarPoderes() {
         Screen screen = new Screen();
         Dialog d = new Dialog();
         String str;
 
-        screen.out("Soy el spiderman de Tom Holland");
+        screen.out("Soy el Spiderman de " + nombre);
         screen.setVisible(true);
-        screen.showImage("TomH.jpg");
+        screen.showImage(poder);
     }
 
 }
