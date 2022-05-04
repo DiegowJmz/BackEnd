d8c058e8d333119190d3be549bdf305f81a96061
Diego I. Jimenez <99154249+DiegowJmz@users.noreply.github.com>
2022-05-04 12:01:09 -0500

    Update TobeySpidy.java

--- PracticaMultiverse/TobeySpidy.java
+++ PracticaMultiverse/TobeySpidy.java
@@ -1,14 +1,21 @@
 public class TobeySpidy extends Spiderman{
 
+    String poder;
+
+    public TobeySpidy(String nombre, String poder) {
+        super(nombre);
+        this.poder = poder;
+    }
+
     @Override
     public void mostrarPoderes() {
         Screen screen = new Screen();
         Dialog d = new Dialog();
         String str;
 
-        screen.out("Soy el spiderman de Tobey Maguire");
+        screen.out("Soy el Spiderman de " + nombre);
         screen.setVisible(true);
-        screen.showImage("tobey.jpg");
+        screen.showImage(poder);
 
     }
 
